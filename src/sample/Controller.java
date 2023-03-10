a6d452e495c237f1d75c5a82dcaeb8fd3bddc787
toptop <eyal.amiel.10@gmail.com>
2023-03-10 23:59:45 +0200

    Initial commit of the chess game

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+import java.awt.*;
+
+public class Controller {
+
+    @FXML
+    private GridPane grid;
+    private Button board;
+    private final int cells = 8;
+
+    public void initialize
+
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    public static int SCREEN_HEIGHT = 720;
+    public static int SCREEN_LENGTH = 720;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, SCREEN_LENGTH, SCREEN_HEIGHT));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
